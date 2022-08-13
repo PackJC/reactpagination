e068425a5d17bddfe89cb09f32b10907a571da5f
PackJC <34726562+PackJC@users.noreply.github.com>
2022-08-13 07:41:28 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+import axios from "axios";
+import Records from "./components/Records";
+import Pagination from "./components/Pagination";
 
 function App() {
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [recordsPerPage] = useState(10);
+    const indexOfLastRecord = currentPage * recordsPerPage;
+    const indexOfFirstRecord = indexOfLastRecord - recordsPerPage;
+    const currentRecords = data.slice(indexOfFirstRecord, indexOfLastRecord);
+    const nPages = Math.ceil(data.length / recordsPerPage);
+
+
+    useEffect(() => {
+    axios.get('MOCK_DATA.json')
+        .then(res => {
+          setData(res.data);
+          setLoading(false);
+        })
+        .catch(() => {
+          alert('There was an error while retrieving the data')
+        })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+        <h2> React: Simple Pagination Example </h2>
+        <Records data={currentRecords}/>
+        <Pagination
+            nPages={nPages}
+            currentPage={currentPage}
+            setCurrentPage={setCurrentPage}
+        />
     </div>
   );
 }
